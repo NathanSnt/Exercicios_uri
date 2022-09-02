99536fb4c60116e829c875a21df58f7f6809fe8a
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-02 10:19:01 -0300

    Término das comparações com if
    falta colocar a apresentação.

--- uri_1018/Program.cs
+++ uri_1018/Program.cs
@@ -21,6 +21,31 @@ namespace uri_1018
                     valor -= 50;
                     nota50 += 1;
                 }
+                else if (valor - 20 >= 0)
+                {
+                    valor -= 20;
+                    nota20 += 1;
+                }
+                else if (valor - 10 >= 0)
+                {
+                    valor -= 10;
+                    nota10 += 1;
+                }
+                else if (valor - 5 >= 0)
+                {
+                    valor -= 5;
+                    nota5 += 1;
+                }
+                else if (valor - 2 >= 0)
+                {
+                    valor -= 2;
+                    nota2 += 1;
+                }
+                else if (valor - 1 >= 0)
+                {
+                    valor -= 1;
+                    nota1 += 1;
+                }
             }
 
         }
