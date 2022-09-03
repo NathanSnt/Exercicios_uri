c7fdfdb853042d7ae2e906e29a0089ec48cbc561
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-03 19:04:20 -0300

    03/09/2022

---
+++ uri_1044/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uri_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+
+            long sobra = 0;
+            Math.DivRem(b, a, out sobra);
+            if (sobra == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
