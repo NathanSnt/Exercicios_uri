ac4e653c3777f14bf088df17e8eabbd8b6e4ea64
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-08 13:57:15 -0300

    08/09/2022 começo de aula

---
+++ uri_1048/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ uri_1065/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
