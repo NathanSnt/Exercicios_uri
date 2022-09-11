458762994df4f990a360704d0e72d4bfa71da69a
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-11 19:42:59 -0300

    11/09/2022

---
+++ uri_1026/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace uri_1026
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    string[] valores = Console.ReadLine().Split();
+                    UInt32 a = uint.Parse(valores[0]);
+                    UInt32 b = uint.Parse(valores[1]);
+                    Console.WriteLine(a ^ b);
+                }
+                catch (Exception EndOfStreamException)
+                {
+                    break;
+                }
+
+                /*
+                string[] valores = Console.ReadLine().Split();
+                BigInteger a = int.Parse(valores[0]);
+                BigInteger b = int.Parse(valores[1]);
+                Console.WriteLine(a ^ b);
+
+                if (a < 0 || b < 0)
+                {
+                    break;
+                }]
+                */
+            }
+        }
+    }
+}
---
+++ uri_1030/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace uri_1030
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int casos, pessoas, salto;
+            casos = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= casos; c++)
+            {
+                string[] valores = Console.ReadLine().Split();
+                List<int> soldados = new List<int>();
+                pessoas = int.Parse(valores[0]);
+                salto = int.Parse(valores[1]);
+
+                for (int i = 1; i <= pessoas; i++)
+                {
+                    soldados.Add(i);
+                }
+
+                int soldado = 0, vivos = soldados.ToArray().Length;
+                while (vivos > 1)
+                {
+                    soldado += salto;
+                    if (soldado > vivos)
+                    {
+                        soldado = 0 + (soldado - vivos);
+                        if (soldado > vivos) { soldado--; }
+                    }
+                    try
+                    {
+                        soldados.RemoveAt(soldado-1);
+                        soldado--;
+                    }
+                    catch
+                    {
+                        if (soldado > vivos)
+                        {
+                            soldado--;
+                            //soldado = 0 + (soldado - vivos);
+                        }
+                        else
+                        {
+                            soldado++;
+                        }
+                    }
+                    vivos--;
+                }
+            Console.WriteLine($"Case {c}: {soldados[0]}");
+            }
+            /*
+            int vivos;
+            int casos = int.Parse(Console.ReadLine());
+            string[] valores;
+            int[] pessoas = {};
+            for (int i = 1; i <= casos; i++) // Número de casos.
+            {
+                valores = Console.ReadLine().Split();
+                int pessoa = int.Parse(valores[0]);
+                int salto = int.Parse(valores[1]);
+                vivos = pessoa;
+                for (int j = 1; j <= pessoa; j++) // Criar vetor com a quantidade de pessoas.
+                {
+                    var lista = pessoas.ToList();
+                    lista.Add(j);
+                    pessoas = lista.ToArray();
+                }
+
+                int morte = 0, loop = 0;
+
+                while (pessoas.Length > 1)
+                {
+                    morte += salto;
+                    if (morte > pessoas.Length) 
+                    { 
+                        if (loop > 1)
+                        {
+                            morte = -1;
+                        }
+                        else
+                        {
+                        morte -= vivos; 
+                        loop++;
+                        }
+                    }
+                    else 
+                    {
+                        var lista = pessoas.ToList();
+                        lista.RemoveAt(morte-1);
+                        pessoas = lista.ToArray();
+                        loop--;
+                        morte--;
+                        vivos--;
+                    }
+                }
+                Console.WriteLine($"Case {i}: {pessoas[0]}");
+            }
+                */
+        }
+    }
+}
