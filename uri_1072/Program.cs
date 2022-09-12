5ac9432b60eadc858ba938231fd1fe31c0ec7520
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-12 13:58:13 -0300

    criação de projeto em sala.

---
+++ uri_1072/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ uri_1143/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
