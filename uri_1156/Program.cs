657885408c55a4dc4d868ca01c51dcb61329fc1d
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-14 02:06:23 -0300

    14/09/2022

---
+++ uri_1156/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri_1156
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            double resultado = 1;
+            int somador = 2;
+
+            for (int i = 3; i <= 39; i += 2)
+            {
+                resultado = resultado + (double)i / somador;
+                somador += somador;
+            }
+            Console.WriteLine(resultado.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+// 6.00
---
+++ uri_3343/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace uri_3343
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int p, m, g, x, n, muralhas = 0, tot = 0;
+            string[] valores, tamanhos;
+            string titas;
+
+            valores = Console.ReadLine().Split();
+            titas = Console.ReadLine();
+            tamanhos = Console.ReadLine().Split();
+
+            n = int.Parse(valores[0]); // Quantidade de titãs
+            x = int.Parse(valores[1]); // Tamanho da muralha
+            p = int.Parse(tamanhos[0]); // Tamanho do titã pequeno
+            m = int.Parse(tamanhos[1]); // Tamanho do titã médio
+            g = int.Parse(tamanhos[2]); // Tamanho do titã grande
+
+            for (int t = 0; t < n; t++)
+            {
+                if (titas[t].Equals('P')) { tot += p; }
+                else if (titas[t].Equals('M')) { tot += m; }
+                else if (titas[t].Equals('G')) { tot += g; }
+            }
+
+            while (tot > 0)
+            {
+                tot -= x;
+                muralhas++;
+            }
+            Console.WriteLine(muralhas);
+        }
+    }
+}
