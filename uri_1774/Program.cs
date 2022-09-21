2d55b184d0d6f46716452b5159ffea2a7bce3277
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-21 17:24:01 -0300

    21/09/2022

--- uri_1774/Program.cs
+++ uri_1774/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace uri_1774
 {
@@ -6,6 +7,23 @@ namespace uri_1774
     {
         static void Main(string[] args)
         {
+            ConcurrentDictionary<int, int[]> dicionario;
+            int roteadores, cabos, R1, R2, custo, custoTotal;
+            string[] valores, entradas;
+            valores = Console.ReadLine().Split();
+
+            roteadores = int.Parse(valores[0]);
+            cabos = int.Parse(valores[1]);
+
+            for (int i = 0; i < cabos; i++)
+            {
+                entradas = Console.ReadLine().Split();
+                R1 = int.Parse(entradas[0]);
+                R2 = int.Parse(entradas[1]);
+                custo = int.Parse(entradas[2]);
+                dicionario = dicionario.AddOrUpdate(R1, custo);
+            }
+            /*
             int roteadores, cabo, R1, R2, valorCabo, custoTot = 0, custo = 10000;
             string[] valores, infos;
 
@@ -14,30 +32,41 @@ namespace uri_1774
             roteadores = int.Parse(valores[0]);
             cabo = int.Parse(valores[1]);
 
+            string[] lista = new string[cabo];
+
             for (int i = 0; i < cabo; i++)
             {
                 infos = Console.ReadLine().Split();
-                for (int r = 1; r <= roteadores; r++)
-                {
+                R1 = int.Parse(infos[0]);
+                R2 = int.Parse(infos[1]);
+                valorCabo = int.Parse(infos[2]);
+                lista[i] = $"{R1} {R2} {valorCabo}";
+            }
 
-                    R1 = int.Parse(infos[0]);
-                    R2 = int.Parse(infos[1]);
-                    valorCabo = int.Parse(infos[2]);
-                    if (r == R1)
+            int controle = 1;
+            for (int i = 0; i < lista.Length; i++)
+            {
+                string r1 = lista[i].Split()[0];
+                string metrica = lista[i].Split()[2];
+                
+                if (int.Parse(r1) == controle)
+                {
+                    if (int.Parse(metrica) < custo)
                     {
-                        if (valorCabo < custo)
-                        {
-                            custo = valorCabo;
-                        }
+                        custo = int.Parse(metrica);
+                        Console.WriteLine($"{lista[i]} = {metrica}");
                     }
-                    else
-                    {
-                        custo = 10000;
-                        custoTot += custo;
-                    }
+                }
+                else
+                {
+                    controle++;
+                    i--;
+                    custoTot += custo;
+                    custo = 10000;
                 }
             }
             Console.WriteLine(custoTot);
+            */
         }
     }
 }
