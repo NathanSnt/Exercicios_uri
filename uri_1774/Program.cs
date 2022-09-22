a943bcc91871caa36ceecfedd8a7b48c32490a37
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-09-22 07:41:24 -0300

    1774

--- uri_1774/Program.cs
+++ uri_1774/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace uri_1774
 {
@@ -7,9 +9,11 @@ namespace uri_1774
     {
         static void Main(string[] args)
         {
-            ConcurrentDictionary<int, int[]> dicionario;
-            int roteadores, cabos, R1, R2, custo, custoTotal;
+            Dictionary<int, List<int>> dicionario = new Dictionary<int, List<int>>();
+
+            int roteadores, cabos, R1, R2, custo, custoTotal = 0;
             string[] valores, entradas;
+            
             valores = Console.ReadLine().Split();
 
             roteadores = int.Parse(valores[0]);
@@ -18,55 +22,32 @@ namespace uri_1774
             for (int i = 0; i < cabos; i++)
             {
                 entradas = Console.ReadLine().Split();
+                
                 R1 = int.Parse(entradas[0]);
                 R2 = int.Parse(entradas[1]);
                 custo = int.Parse(entradas[2]);
-                dicionario = dicionario.AddOrUpdate(R1, custo);
+
+                if (!dicionario.ContainsKey(R1)) { dicionario[R1] = new List<int>(); }
+                dicionario[R1].Add(custo);
+
+                if (!dicionario.ContainsKey(R2)) { dicionario[R2] = new List<int>(); }
+                dicionario[R2].Add(custo);
             }
-            /*
-            int roteadores, cabo, R1, R2, valorCabo, custoTot = 0, custo = 10000;
-            string[] valores, infos;
 
-            valores = Console.ReadLine().Split();
+            List<List<int>> conectados;
 
-            roteadores = int.Parse(valores[0]);
-            cabo = int.Parse(valores[1]);
-
-            string[] lista = new string[cabo];
-
-            for (int i = 0; i < cabo; i++)
+            //for (int i = 1; i <= roteadores; i++)
+            int c = 1;
+            while (c <= roteadores)
             {
-                infos = Console.ReadLine().Split();
-                R1 = int.Parse(infos[0]);
-                R2 = int.Parse(infos[1]);
-                valorCabo = int.Parse(infos[2]);
-                lista[i] = $"{R1} {R2} {valorCabo}";
+
+
+                custoTotal += dicionario[c].Min();
+                Console.WriteLine($"{dicionario[c].Min()} é o menor link de R{c}");
+                c++;
             }
 
-            int controle = 1;
-            for (int i = 0; i < lista.Length; i++)
-            {
-                string r1 = lista[i].Split()[0];
-                string metrica = lista[i].Split()[2];
-                
-                if (int.Parse(r1) == controle)
-                {
-                    if (int.Parse(metrica) < custo)
-                    {
-                        custo = int.Parse(metrica);
-                        Console.WriteLine($"{lista[i]} = {metrica}");
-                    }
-                }
-                else
-                {
-                    controle++;
-                    i--;
-                    custoTot += custo;
-                    custo = 10000;
-                }
-            }
-            Console.WriteLine(custoTot);
-            */
+            Console.WriteLine(custoTotal);
         }
     }
 }
