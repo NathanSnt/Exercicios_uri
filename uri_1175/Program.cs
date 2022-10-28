a07a14642b6c3355b426e5904962157a2ac314c7
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-10-27 23:42:15 -0300

    uri_1175

---
+++ uri_1175/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+
+namespace uri_1175
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vetor = new int[20];
+
+            for (int x = 0; x < 20; x++)
+            {
+                vetor[x] = int.Parse(Console.ReadLine());
+            }
+
+            int helper;
+            for (int x = 0; x < 10; x++)
+            {
+                helper = vetor[x];
+                vetor[x] = vetor[19 - x];
+                vetor[19 - x] = helper;
+            }
+
+            for (int x = 0; x < 20; x++)
+            {
+                Console.WriteLine($"N[{x}] = {vetor[x]}");
+            }
+        }
+    }
+}
