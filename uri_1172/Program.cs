f9b32ddaad7ce72b5344c19eb20e720b43ef19aa
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-10-27 23:21:11 -0300

    uri_1172

---
+++ uri_1172/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri_1172
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            int[] valores = new int[10];
+            for (int x = 0; x < 10; x++)
+            {
+                valor = int.Parse(Console.ReadLine());
+                valores[x] = valor > 0 ? valor : 1;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"X[{i}] = {valores[i]}");
+            }
+        }
+    }
+}
