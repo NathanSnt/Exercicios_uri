e8731c681b929d2bc3f00a2e810a03acfd4cd246
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-10-27 23:21:02 -0300

    uri_1173

---
+++ uri_1173/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace uri_1173
+{
+    using System;
+
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            int[] N = new int[10];
+
+            N[0] = valor;
+
+            for (int x = 1; x < 10; x++)
+            {
+                N[x] = N[x-1] + N[x-1];
+            }
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine($"N[{x}] = {N[x]}");
+            }
+        }
+    }
+}
