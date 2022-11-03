89805d01c4a5a96efbf5fbd5695d302c88553258
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-11-02 22:50:29 -0300

    Tacógrafo.

---
+++ uri_2388/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri_2388
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int distancia = 0;
+            string[] valores;
+            int N = int.Parse(Console.ReadLine());
+
+            for (int x = 0; x < N; x++)
+            {
+                valores = Console.ReadLine().Split();
+                distancia += int.Parse(valores[1]) * int.Parse(valores[0]);
+            }
+            Console.WriteLine(distancia);
+        }
+    }
+}
