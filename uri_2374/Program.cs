b63f240b494b14758587043fd124b89ccebac330
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-11-02 21:32:38 -0300

    Calibrar pressão em um pneu.

---
+++ uri_2374/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uri_2374
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int pressaoDesejada = int.Parse(Console.ReadLine());
+            int pressaoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(pressaoDesejada - pressaoAtual);
+        }
+    }
+}
