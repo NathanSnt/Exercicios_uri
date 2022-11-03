d60a2dfdb791ac6f92c3dd6dff58df314d92b92f
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-11-02 21:54:29 -0300

    Sequência secreta na calçada em frente ao Palácio Imperial

---
+++ uri_3048/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uri_3048
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] V = new int[N];
+            int i = 0, x = 0;
+
+            for (int v = 0; v < N; v++)
+            {
+                V[v] = int.Parse(Console.ReadLine());
+            }
+
+            for (int v = 0; v < N; v++)
+            {
+                x = (v == 0)? 0 : V[v-1];
+                if (x != V[v])
+                {
+                    i++;
+                }
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
