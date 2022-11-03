884fd1d3352c745fcb5b25e02ee76577944d24e7
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-11-02 21:28:35 -0300

    Quantidade de clicks.

---
+++ uri_2413/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace uri_2413
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            int clicksTerceiroLink = int.Parse(Console.ReadLine());
+            Console.WriteLine(clicksTerceiroLink * 4);
+        }
+    }
+}
