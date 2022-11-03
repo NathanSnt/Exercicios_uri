75cc94f873f96f905e9472a5d926853fbcb9ccb0
Nathan Santos <79227411+NathanSnt@users.noreply.github.com>
2022-11-02 21:18:05 -0300

    Desafio dos Chás.

---
+++ uri_2006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri_2006
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            string tipo = Console.ReadLine();
+            string[] valores = Console.ReadLine().Split();
+
+            int acertos = 0;
+
+            for (int x = 0; x < 5; x++)
+            {
+                if (valores[x] == tipo)
+                {
+                    acertos++;
+                }
+            }
+            Console.WriteLine(acertos);
+        }
+    }
+}
